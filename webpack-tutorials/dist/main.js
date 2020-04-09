9cec6d45c9a855e8babe529f2f32dd09fc54878f
Estelle Choi <estele.choi@gmail.com>
2020-04-09 15:50:11 +0900

    npm run build returns /dist

--- webpack-tutorials/dist/main.js
+++ webpack-tutorials/dist/main.js
@@ -92,11 +92,13 @@
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
 /* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);
+// var _ = require('lodash');
 
 
 function component() {
 	var element = document.createElement("div");
 	// lodash is required for the next line to work
+	// _.join(array, separator)
 	element.innerHTML = lodash__WEBPACK_IMPORTED_MODULE_0___default.a.join(["hello", "webpack"], " ");
 
 	return element;
