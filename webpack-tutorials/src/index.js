639ccfd11750fb39e2e4a3738a75774313f4ba2f
Estelle Choi <estele.choi@gmail.com>
2020-04-09 19:08:51 +0900

    npm run build 를 하지않고 웹팩 데브 서버로 결과 확인하기

--- webpack-tutorials/src/index.js
+++ webpack-tutorials/src/index.js
@@ -1,14 +1,17 @@
 // var _ = require('lodash');
-import _ from "lodash";
+// import _ from "lodash";
 import "./../base.css";
 
-function component() {
-	var element = document.createElement("div");
-	// lodash is required for the next line to work
-	// _.join(array, separator)
-	element.innerHTML = _.join(["hello", "webpack"], " ");
+// function component() {
+// 	var element = document.createElement("div");
+// 	// lodash is required for the next line to work
+// 	// _.join(array, separator)
+// 	element.innerHTML = _.join(["hello", "webpack"], " ");
 
-	return element;
-}
+// 	return element;
+// }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
+
+var div = document.querySelector(".container");
+div.innerText = "Webpack loaded!!";
--- webpack-tutorials/webpack.config.js
+++ webpack-tutorials/webpack.config.js
@@ -4,6 +4,7 @@
 var path = require("path");
 // mini-css-extract-plugin 라이브러리 가져오기
 var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 	mode: "none", // production, development
@@ -28,7 +29,15 @@ module.exports = {
 			},
 		],
 	},
-	plugins: [new MiniCssExtractPlugin()],
+	devServer: {
+		port: 9000,
+	},
+	plugins: [
+		new MiniCssExtractPlugin(),
+		new HtmlWebpackPlugin({
+			template: "index.html", // index.html 템플릿을 기반으로 빌드 결과물을 추가
+		}),
+	],
 	// source-map makes it possible to refer to original source before minified
 	// devtool: "source-map",
 };
